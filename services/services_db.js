63349c8d8870e8eb0427eb88835a6ec9b86c08e7
rodrigo.vasconcelos <rodrigo.vasconcelos@gpssa.com.br>
2025-04-28 18:12:02 -0300

    Add MongoDB connection using MongoClient and update package dependencies

--- services/services_db.js
+++ services/services_db.js
@@ -1,9 +1,26 @@
-const mongoose = require('mongoose');
 
-module.exports = () => {
-  mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }).then(() => console.log('MongoDB conectado'))
-    .catch(err => console.error('Erro ao conectar MongoDB:', err));
-};
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://rodrigovmoreira:RdgRock810@cluster0.gb9aazt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
